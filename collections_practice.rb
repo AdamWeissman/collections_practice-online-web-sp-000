815bd89639ef8f9610af02b6d25d53cae3f0c394
Adam Weissman <adam@comedytragedyepic.com>
2019-09-18 00:58:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc(an_array)
+  return an_array.sort
+end
 
+def sort_array_desc(an_array)
+  return sort_array_asc(an_array).reverse
+end
+
+def sort_array_char_count(an_array)
+  word = an_array.split.to_a
+  
