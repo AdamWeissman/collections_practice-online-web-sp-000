8623f52821d427a7e3fb89759b2c536b1d82d4bc
Adam Weissman <adam@comedytragedyepic.com>
2019-09-18 00:59:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,7 @@ def sort_array_desc(an_array)
   return sort_array_asc(an_array).reverse
 end
 
-def sort_array_char_count(an_array)
-  word = an_array.split.to_a
-  
+def sort_array_char_count(string_array)
+  return string_array.sort_by {|word| word.length}
+end
+
