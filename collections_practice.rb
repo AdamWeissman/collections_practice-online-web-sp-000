5d0437fe2fa9ba4af67b01e75ad2a2b2c36659b6
Adam Weissman <adam@comedytragedyepic.com>
2019-09-18 01:48:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,7 +29,18 @@ def kesha_maker(make_kesha)
  end
 end
 
-def find_a(starts_with)
-  return 
+def find_a(an_array)
+  words_that_start_with_a = []
+  counter = 0
+  while counter < an_array.length
+  an_array.each do |word|
+    if word.start_with("a")
+      words_that_start_with_a << word
+      counter += 1 
+    else
+      counter += 1
+      next
+    end
+  end
 end
     
